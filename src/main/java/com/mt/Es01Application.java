8a13d5f020186d1cceea1dc8aa8b4f94c23b6181
guoJunWang <1994371810@qq.com>
2020-10-24 17:48:03 +0800

    提交笔记

--- src/main/java/com/mt/Es01Application.java
+++ src/main/java/com/mt/Es01Application.java
@@ -3,6 +3,11 @@ package com.mt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* 根据 视频教程 写的 具体参考
+*
+* https://www.bilibili.com/video/BV1Qz411e7yx
+* */
 @SpringBootApplication
 public class Es01Application {
 
